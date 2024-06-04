0d8e3be2f5b53da7bf47b15c66d44a859a23cf77
sunmoon1992 <sunmoon19920211@gmail.com>
2024-06-04 19:12:31 +0800

    fix

--- src/components/Logo/styled.ts
+++ src/components/Logo/styled.ts
@@ -16,7 +16,7 @@ export const _Group = styled.div<{ $size?: number, $mLeft?: number }>`
 
 export const _Logo = styled.div<{ $size?: number, $radius: number, $zIndex: number, $border?: boolean, $borderColor?: string }>`
   z-index: ${(props) => props.$zIndex};
-  border-radius: ${(props) => props?.$radius >= 0 ? props?.$radius + 'px' : '100%'};
+  border-radius: ${(props) => props?.$radius || '100%'};
   border-width: ${(props) => props?.$border ? 2 : 0}px;
   border-color: ${(props) => props?.$borderColor ?? '#222'};
   background-color: ${(props) => props?.$borderColor ?? '#222'};
